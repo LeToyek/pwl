2e93aa98f3f87fea58249098883c9fc640547a0f
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-02-22 14:51:30 +0700

    praktikum 2 no 3

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    function index(){
+        return "NIM\t: 2141720085 | Nama\t: Maulana Arif Wijaya";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[PageController::class,'index']);
-Route::get('/about',[PageController::class,'about']);
-Route::get('/articles/{id}',[PageController::class,'articles']);
+Route::get('/',[HomeController::class,'index']);
+Route::get('/about',[AboutController::class,'index']);
+Route::get('/articles/{id}',[ArticleController::class,'index']);
